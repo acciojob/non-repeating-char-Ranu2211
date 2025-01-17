32a066e281734800a324d23ae663a12a77a8f0f2
Ranu <88536226+Ranu2211@users.noreply.github.com>
2025-01-17 17:09:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	for(const char of str){
+		if(str.indexOf(char) === str.lastIndexOf(char)){
+		  return char;
+		}
+	}
+return null;
 }
+
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
